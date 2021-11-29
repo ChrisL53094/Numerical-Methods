6427766e4f551a7ce5e7451c77efc3f78992617c
Christopher Leonard <cleonar@ncsu.edu>
2021-11-28 23:13:53 -0500

    changes Matrix size

--- Numerical_LA/src/Main.java
+++ Numerical_LA/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 	public static void main(String[] args)
     {
-		Matrix M = new Matrix(3,4);
+		Matrix M = new Matrix(4,2);
 		int[] size = M.getSize();
 		System.out.println("matrix size is "+size[0]+" by "+size[1]);
     }
